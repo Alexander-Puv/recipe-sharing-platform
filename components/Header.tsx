c0307564749141ed5a3b90536fd2dd2d976c7dd4
Alexander-Puv <sansano4sana@gmail.com>
2024-07-23 22:33:10 +0300

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
-  const isLoggedIn = true
+  const isLoggedIn = false
 
   return (
     <header className='header'>
