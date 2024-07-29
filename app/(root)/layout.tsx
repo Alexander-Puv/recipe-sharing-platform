974a29cfa3662b9468f2f4558b2b236fafab7a59
Alexander-Puv <sansano4sana@gmail.com>
2024-07-29 16:49:29 +0300

    footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 export default function RootLayout({
@@ -7,5 +8,7 @@ export default function RootLayout({
 }>) {
   return (<>
     <Header />
+    {children}
+    <Footer />
   </>);
 }
---
+++ components/svg/index.ts
@@ -0,0 +1,12 @@
+import BroccoliSvg from "./BroccoliSvg";
+import CarrotSvg from "./CarrotSvg";
+import LemonSvg from "./LemonSvg";
+import PepperSvg from "./PepperSvg";
+import PumpkinSvg from "./PumpkinSvg";
+import TomatoSvg from "./TomatoSvg";
+
+export interface SvgProps {
+  className?: string
+}
+
+export {BroccoliSvg, CarrotSvg, LemonSvg, PepperSvg, PumpkinSvg, TomatoSvg}
